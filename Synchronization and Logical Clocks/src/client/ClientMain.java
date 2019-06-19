37796ebfecf810c78ec461fe4e927a7e4629cd24
Sameer Valiah Pusunuru <sameerpvs@gmail.com>
2019-06-19 17:13:24 -0500

    Add files via upload

---
+++ Synchronization and Logical Clocks/src/client/ClientMain.java
@@ -0,0 +1,23 @@
+/* Sameer Valiah Pusunuru
+ * 1001552473
+ * DS Lab1
+ */
+
+package client;
+//for ui of the client
+import javax.swing.*;
+//class to initiate client 
+public class ClientMain {
+
+	public static void main(String[] args) throws Exception{
+		//calling instance of the client interface
+		ClientGUI client = new ClientGUI();
+		//Enabling the close button feature in the client user interface
+		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//starting client operations
+		client.startClient();
+		
+		
+		
+	}
+}
---
+++ Synchronization and Logical Clocks/src/server/ServerMain.java
@@ -0,0 +1,19 @@
+/* Sameer Valiah Pusunuru
+ * 1001552473
+ * DS Lab1
+ */
+
+package server;
+//nedded for server gui
+import javax.swing.*;
+
+public class ServerMain {
+	public static void main (String[] args){
+		//intiating a server client gui instancece
+		ServerGUI s = new ServerGUI();
+		//enabling the function to close the window and exit on the gui
+		s.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//method in which server will be created and handled
+		s.startRunning();  
+	}
+}

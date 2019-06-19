83ebcf835af0c8dac441cc02747e04baa92b39c0
Sameer Valiah Pusunuru <sameerpvs@gmail.com>
2019-06-19 17:16:36 -0500

    Add files via upload

---
+++ Push and Pull/src/notification/NotificationMain.java
@@ -0,0 +1,22 @@
+/* Sameer Valiah Pusunuru
+ * 1001552473
+ * DS Lab3
+ */
+package notification;
+
+import javax.swing.JFrame;
+
+
+//notification process
+public class NotificationMain {
+
+	//intializing notification
+	public static void main(String[] args) throws Exception{
+		//making an object of notification to get gui
+	NotificationGUI noti = new NotificationGUI();
+	//making gui able to close by pressing the red cross button and exiting the process
+	noti.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	//starting the notification process
+	noti.startClient();
+}
+}
---
+++ Push and Pull/src/student/StudentMain.java
@@ -0,0 +1,23 @@
+/* Sameer Valiah Pusunuru
+ * 1001552473
+ * DS Lab3
+ */
+
+package student;
+//for ui of the client
+import javax.swing.*;
+//class to initiate client 
+public class StudentMain {
+
+	public static void main(String[] args) throws Exception{
+		//calling instance of the client interface
+		StudentGUI client = new StudentGUI();
+		//Enabling the close button feature in the client user interface
+		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//starting client operations
+		client.startClient();
+		
+		
+		
+	}
+}
